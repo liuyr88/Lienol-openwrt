5a24222d407334133c83f501f43bc43c1862ec04
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2025-01-10 10:38:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,10 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Melt-OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/root:::0:99999:7:::/root:$1$m9enpRnu$TjbaRy7zgCDF.hXF6md7p1:20077:0:99999:7:::/g' package/base-files/files/etc/shadow
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
